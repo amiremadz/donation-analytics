a89839aed556b881b00cea398d16550bbca10e40
Amir Emadzadeh <aemadzadeh@nvidia.com>
2018-02-15 19:27:10 -0800

    Post-processing.

--- src/donation-analytics.py
+++ src/donation-analytics.py
@@ -3,30 +3,20 @@ import csv
 import numpy as np
 import pandas as pd
 import datetime as dtm
+import math
 
 data_file = "itcont.txt"
 pcnt_file = "percentile.txt"
 
-output_file = "repeat_donors_amir.txt"
+output_file = "repeat_donors.txt"
 
-output_path = "../insight_testsuite/tests/test_1/output"
-input_path = "../insight_testsuite/tests/test_1/input/"
+output_path = "../output"
+input_path = "../input/"
 
 cols = [0, 7, 10, 13, 14, 15]
 col_names = ['CMTE_ID', 'NAME', 'ZIP_CODE', 'TRANSACTION_DT', 'TRANSACTION_AMT', 'OTHER_ID']
+out_cols  = ['CMTE_ID', 'ZIP_CODE', 'TRANSACTION_YR', 'QUANTILE', 'SUM_SOFAR', 'COUNT_SOFAR']
 
-with open(os.path.join(input_path, pcnt_file)) as fh:
-	percentile = fh.read()
-
-percentile = float(percentile.strip()) 
-
-df = pd.read_csv(os.path.join(input_path, data_file), header=None, delimiter='|')
-df = df[cols]
-df.columns = col_names
-
-# Because we are only interested in individual contributions, 
-# we only want records that have the field, OTHER_ID, set to empty 
-df = df[df["OTHER_ID"].isnull()]
 
 def validate_date(df):
 	length = df.shape[0]
@@ -38,28 +28,10 @@ def validate_date(df):
 			valid.iloc[idx] = False
 	return df[valid]
 
-# If TRANSACTION_DT is an invalid date (e.g., empty, malformed)
-df = df[~ df["TRANSACTION_DT"].isnull()]
-df["TRANSACTION_DT"] = df["TRANSACTION_DT"].map(lambda td: str(td))
-df = validate_date(df)
-
-# Output: 4-digit year of the contribution
-df["TRANSACTION_YR"] = df["TRANSACTION_DT"].map(lambda td: td[-4:])
-
 def validate_zipcode(df):
 	length = df.shape[0]
 	valid = df["ZIP_CODE"].map(lambda s: len(s) >= 5)
 	return df[valid]
-
-# If ZIP_CODE is an invalid zip code (i.e., empty, fewer than five digits)
-df = df[~ df["ZIP_CODE"].isnull()]
-df["ZIP_CODE"] = df["ZIP_CODE"].map(lambda z: str(z))
-df = validate_zipcode(df)
-
-# While the data dictionary has the ZIP_CODE occupying nine characters, 
-# for the purposes of the challenge, we only consider the first five characters 
-# of the field as the zip code
-df["ZIP_CODE"] = df["ZIP_CODE"].map(lambda z: z[0:5])
 
 def validate_name(df):
 	length = df.shape[0]
@@ -69,13 +41,76 @@ def validate_name(df):
 			valid.iloc[idx] = False	
 	return df[valid]
 
-# If the NAME is an invalid name (e.g., empty, malformed)
-df = df[~ df["NAME"].isnull()]
-df = validate_name(df)
+def find_precentile(input_path, pcnt_file):
+	with open(os.path.join(input_path, pcnt_file)) as fh:
+		percentile = fh.read()
+	percentile = float(percentile.strip()) 
+	return percentile
 
-# If any lines in the input file contains empty cells in the CMTE_ID 
-# or TRANSACTION_AMT fields
-df = df[~ df["CMTE_ID"].isnull()]
-df = df[~ df["TRANSACTION_AMT"].isnull()]
+def preprocess(input_path, data_file):	
+	df = pd.read_csv(os.path.join(input_path, data_file), header=None, delimiter='|')
+	df = df[cols]
+	df.columns = col_names
 
-print(df)
+	# Because we are only interested in individual contributions, 
+	# we only want records that have the field, OTHER_ID, set to empty 
+	df = df[df["OTHER_ID"].isnull()]
+
+	# If TRANSACTION_DT is an invalid date (e.g., empty, malformed)
+	df = df[~ df["TRANSACTION_DT"].isnull()]
+	df["TRANSACTION_DT"] = df["TRANSACTION_DT"].map(lambda td: str(td))
+	df = validate_date(df)
+
+	# Output: 4-digit year of the contribution
+	df["TRANSACTION_YR"] = df["TRANSACTION_DT"].map(lambda td: td[-4:])
+
+	# If ZIP_CODE is an invalid zip code (i.e., empty, fewer than five digits)
+	df = df[~ df["ZIP_CODE"].isnull()]
+	df["ZIP_CODE"] = df["ZIP_CODE"].map(lambda z: str(z))
+	df = validate_zipcode(df)
+
+	# While the data dictionary has the ZIP_CODE occupying nine characters, 
+	# for the purposes of the challenge, we only consider the first five characters 
+	# of the field as the zip code
+	df["ZIP_CODE"] = df["ZIP_CODE"].map(lambda z: z[0:5])
+
+	# If the NAME is an invalid name (e.g., empty, malformed)
+	df = df[~ df["NAME"].isnull()]
+	df = validate_name(df)
+
+	# If any lines in the input file contains empty cells in the CMTE_ID 
+	# or TRANSACTION_AMT fields
+	df = df[~ df["CMTE_ID"].isnull()]
+	df = df[~ df["TRANSACTION_AMT"].isnull()]
+	df = df[df["TRANSACTION_AMT"] > 0]
+
+	# Drop redundant columns
+	df = df.drop(['TRANSACTION_DT', 'OTHER_ID'], axis=1)
+	return df
+
+def postprocess(df):
+	df = df[df.duplicated(subset = {'NAME', 'ZIP_CODE'}, keep='first')]
+	df = df.reset_index(drop=True)
+		
+	df['SUM_SOFAR']   = df.groupby(['CMTE_ID', 'ZIP_CODE', 'TRANSACTION_YR'])['TRANSACTION_AMT'].cumsum()
+	df['COUNT_SOFAR'] = df.groupby(['CMTE_ID', 'ZIP_CODE', 'TRANSACTION_YR'])['TRANSACTION_AMT'].cumcount().apply(lambda x: x + 1)
+	df['QUANTILE'] = np.nan
+
+	for idx, _ in enumerate(df['TRANSACTION_AMT']):
+		idx_quantile = int(math.ceil((idx + 1) * (percentile / 100.0))-1)
+		df['QUANTILE'][idx] = round(df['TRANSACTION_AMT'][:idx+1].sort_values(inplace=False)[idx_quantile])
+
+	df['QUANTILE'] = df['QUANTILE'].astype('int')
+
+	# Drop redundant columns and re-arrange to correct format
+	df = df.drop(['NAME', 'TRANSACTION_AMT'], axis=1)
+	df = df[out_cols]
+
+	return df
+
+if __name__ == "__main__":
+	percentile = find_precentile(input_path, pcnt_file)
+	df = preprocess(input_path, data_file)
+	df = postprocess(df)
+	print(df)
+	df.to_csv(os.path.join(output_path, output_file), sep='|', header=False, index=False)
